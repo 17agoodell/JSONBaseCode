50ccefc4891b426c97e0b9aa011c265dbd534a7e
17agoodell <17agoodell@cr.k12.ia.us>
2016-11-29 08:21:24 -0600

    JSON Beginner Coding
    I'm learning how to parse JSON code into Unity. Next step: connect JSON
    objects & arrays to text in Unity.

---
+++ Assets/ReadJson.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.IO;
+using LitJson;
+
+public class ReadJson : MonoBehaviour {
+	private string jsonstring;
+	private JsonData itemData;
+
+	// Use this for initialization
+	void Start () {
+		jsonstring = File.ReadAllText (Application.dataPath + "/JsonText.json");
+		itemData = JsonMapper.ToObject(jsonstring);
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
